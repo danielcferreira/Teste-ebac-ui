65ff552a3f4af834beb706cc027e43b063705803
Daniel Chagas <daniel.cferreira04@gmail.com>
2022-02-08 17:17:16 -0300

    Atualizacao dos teste automaticos

--- cypress/integration/pre-cadastro.spec.js
+++ cypress/integration/pre-cadastro.spec.js
@@ -11,14 +11,18 @@ describe('Funcionalidade Pré Cadastro', () =>{
     });
 
     it('Deve completar o pre casatro com sucesso',()=>{
-        cy.get('#reg_email').type(faker.internet.email())
+           let primeiroNome = faker.name.firstName()
+           let sobreNome = faker.name.lastName()
+           let emailFaker = faker.internet.email(primeiroNome)
+
+        cy.get('#reg_email').type(emailFaker)
         cy.get('#reg_password').type('!teste@teste$')
         cy.get(':nth-child(4) > .button').click()
 
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
 
-        cy.get('#account_first_name').type(faker.name.firstName())
-        cy.get('#account_last_name').type(faker.name.lastName())
+        cy.get('#account_first_name').type(primeiroNome)
+        cy.get('#account_last_name').type(sobreNome)
         cy.get('.woocommerce-Button').click()
 
         cy.get('.woocommerce-message').should('contain','Detalhes da conta modificados com sucesso.')
